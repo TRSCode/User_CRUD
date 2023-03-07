79a09eee27cf8aeedb8966b252ff2f30537b535f
Tony Schock <tony.schock@gmail.com>
2023-03-07 10:50:28 -0700

    first commit

---
+++ server.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template, request, redirect
+from user import User
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    users = User.get_all()
+    print(users)
+    return render_template("index.html", all_users = users)
+
+@app.route('/add_user', methods=["POST"])
+def add_user():
+    data = {
+        "first_name": request.form["first_name"],
+        "last_name" : request.form["last_name"],
+        "email" : request.form["email"]
+    }
+    User.save(data)
+    return redirect('/')
+
+@app.route('/create')
+def create():
+    return render_template('/create.html')
+
+@app.route('/update/<int:id>')
+def edit(id):
+    data = {"id":id}
+    return render_template("update.html",user=User.get_one(data))
+
+@app.route('/show_user/<int:id>')
+def show_user(id):
+    data = {"id":id}
+    return render_template("show_user.html",user=User.get_one(data))
+
+@app.route('/update', methods=['POST'])
+def update():
+    User.update(request.form)
+    return redirect('/')
+
+@app.route('/remove/<int:id>')
+def remove(id):
+    data ={'id':id}
+    User.remove(data)
+    return redirect('/') 
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ user.py
@@ -0,0 +1,59 @@
+from mysqlconnection import connectToMySQL
+
+class User:
+    DB = "users_schema"
+    def __init__( self , data ):
+        self.id = data['id']
+        self.first_name = data['first_name']
+        self.last_name = data['last_name']
+        self.email = data['email']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+
+    @classmethod
+    def get_all(cls):
+        query = """SELECT * 
+        FROM users;"""
+        results = connectToMySQL(cls.DB).query_db(query)
+        users = []
+        for row in results:
+            users.append( cls(row))
+        return users
+
+    @classmethod
+    def save(cls, data ):
+        query = """
+                INSERT INTO 
+                    users ( first_name , last_name , email , created_at, updated_at ) 
+                VALUES 
+                    ( %(first_name)s , %(last_name)s , %(email)s , NOW() , NOW() );
+                """
+        return connectToMySQL(cls.DB).query_db( query, data )
+    
+    @classmethod
+    def get_one(cls,data):
+        query = """
+                SELECT * 
+                FROM users
+                WHERE id = %(id)s; 
+        """
+        result = connectToMySQL(cls.DB).query_db( query, data )
+        return cls(result[0])
+    
+    @classmethod
+    def update(cls, data):
+        query = """
+                UPDATE users
+                SET first_name=%(first_name)s, last_name=%(last_name)s, email=%(email)s, updated_at=NOW()
+                WHERE id = %(id)s;
+        """
+        return connectToMySQL(cls.DB).query_db( query, data )
+
+
+    @classmethod
+    def remove(cls, data):
+        query = """
+                DELETE FROM users
+                WHERE id = %(id)s;
+        """
+        return connectToMySQL(cls.DB).query_db( query, data )
